665f399d42368e4b6186639f9b5ce4eeffa01148
Steven Pataray <spataray@gmail.com>
2026-03-29 12:37:23 -1000

    Cleanup Layout: Removed Up Next toast for repositioning (v3.0.9)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// v3.0.8 (2026-03-28 23:00 HST): Sharp gold 'Up Next' toast pinned to right edge.
+// v3.0.9 (2026-03-28 23:15 HST): Removed Up Next for repositioning.
 // Karaplay - Main Logic (Legacy ES5 for Car Compatibility)
 
 var player;
